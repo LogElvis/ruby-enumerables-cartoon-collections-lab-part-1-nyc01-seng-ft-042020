3469fd6a0931072e631b5c08f5d8702e24ded0df
Elvis Ning <ningelvis@gmail.com>
2020-04-12 19:31:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,7 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index do |value, index|
+    array[value] = index
     puts "#{index}. #{value}"
   end
 end
